d4f36fda85aae20cbd49f62d753c7f99bc30b21c
sh8941 <zshamoonhaider@gmail.com>
2026-05-15 09:49:21 +0530

    first commit

---
+++ src/main/java/com/haider/TeamWorkTracker/TeamWorkTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.haider.TeamWorkTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamWorkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamWorkTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haider/TeamWorkTracker/TeamWorkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haider.TeamWorkTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
